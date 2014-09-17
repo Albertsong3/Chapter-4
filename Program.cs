964766f6651901dbd62052d2dd0d3a5150665122
Albertsong3 <gregalbertson@rocketmail.com>
2014-09-17 08:38:28 -0400

    done with 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Any number thats less than 999");
+           int Total = 0;
+           while (true)
+           {
+               int I = Convert.ToInt32(Console.ReadLine()); 
+               Total += I;
+               Console.WriteLine("   " + Total);
+               if (Total >= 999)
+               break;
+           }
+
+
+            
+
+          
+
+        }
+    }
+}
