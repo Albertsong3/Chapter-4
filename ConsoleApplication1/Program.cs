26e7a18ca87e5edcc50659b418ee949d98c0467a
Albertsong3 <gregalbertson@rocketmail.com>
2014-09-19 08:43:19 -0400

    on 5

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char user;
+            string input;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("Enter a vowel");
+                input = Console.ReadLine();
+                user = Convert.ToChar(input);
+
+
+
+
+                if (user == '!')
+                    break;
+                switch (user)
+                {
+                    case 'a': Console.WriteLine("Ok");
+                        break;
+
+                    case 'e': Console.WriteLine("Ok");
+                        break;
+
+                    case 'i': Console.WriteLine("Ok");
+                        break;
+
+                    case 'o': Console.WriteLine("Ok");
+                        break;
+
+                    case 'u': Console.WriteLine("Ok");
+                        break;
+
+                    case 'A': Console.WriteLine("Ok");
+                        break;
+
+                    case 'E': Console.WriteLine("Ok");
+                        break;
+
+                    case 'I': Console.WriteLine("Ok");
+                        break;
+
+                    case 'O': Console.WriteLine("Ok");
+                        break;
+
+                    case 'U': Console.WriteLine("Ok");
+                        break;
+
+                    default: Console.WriteLine("Error");
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Multiplcation table/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Multiplcation_table
+{
+    class Program
+    {*
+        static void Main(string[] args)*
+        { 
+            for ( i = 1;i <=10;i++)
+            Console.WriteLine("(0,4)|(1,4)|(2,4)|(3,4)|(4,4)|(5,4)|(6,4)|(7,4)|(8,4)|(9,4)|(10,4)", i , i*1, i*2, i*3, i*4, i*5, i*6, i*7, i*8, i*9, i*10); 
+        }
+    }
+}
