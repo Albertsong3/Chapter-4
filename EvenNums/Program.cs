2b902e50ac0b598f4d4bd27d715575892955ad78
Albertsong3 <gregalbertson@rocketmail.com>
2014-09-24 08:44:22 -0400

    on 7

---
+++ EvenNums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2;
+            int red;
+
+            while (num <= 100)
+            {
+                red = num % 2;
+                if (red.Equals(0))
+                    Console.WriteLine(num);
+
+                num++;
+            }
+
+
+            
+            
+
+           
+        }
+    }
+}
