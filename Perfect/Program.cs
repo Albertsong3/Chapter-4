4193c44ee5e0290c96fbcb16dff3a287cbb496fc
Albertsong3 <gregalbertson@rocketmail.com>
2014-09-29 08:42:46 -0400

    done

---
+++ Perfect/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Perfect
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int Number = 1, counter, sum = 0, mod;
+
+            while (Number <= 1000)
+            {
+                counter = Number - 1;
+
+                while (counter > 0)
+                {
+                    mod = Number % counter;
+
+                    if (mod.Equals(0))
+                        sum = sum + counter;
+                    counter--;
+
+                }
+
+                if (sum.Equals(Number))
+                    Console.WriteLine(Number);
+
+                sum = 0;
+
+                Number++;
+            }
+            
+
+        }
+    }
+}
